06007c5a4a5c8145306c350e0c522a06ee1a9bd9
Kevin Nguyen <kevinngx625@gmail.com>
2021-03-24 16:01:08 +1100

    Day 21

---
+++ Day_21/day-21-exercise/animal.py
@@ -0,0 +1,21 @@
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+
+    def breathe(self):
+        print("Inhale, exhale.")
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def swim(self):
+        print("Moving in water.")
+    
+    def breathe(self):
+        super().breathe()
+        print("doing this underwater")
+
+nemo = Fish()
+nemo.swim()
+nemo.breathe()
