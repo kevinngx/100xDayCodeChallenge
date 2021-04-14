704d4fe1ebf4a5d8ac105564a8e3106184fa3242
kevinngx <kevinngx625@gmail.com>
2021-04-14 10:54:52 +1000

    Day 27

--- Day_27/day-27-learning/main.py
+++ Day_27/day-27-learning/main.py
@@ -6,14 +6,12 @@ window.minsize(width=500, height=300)
 window.config(padx=20, pady=20)
 
 # 1. Label
-
 my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
 # my_label.pack()
 my_label.grid(column=1, row=1)
 my_label["text"] = "This is a new text value"
 
 # 2. Button
-
 def button_clicked():
     # my_label["text"] = "I got clicked"
     my_label.config(text=input.get())
@@ -26,7 +24,6 @@ new_button = Button(text="New Button", command=button_clicked)
 new_button.grid(column=3, row=1)
 
 # 3. Entry
-
 def print_input():
     print(input.get())
 
