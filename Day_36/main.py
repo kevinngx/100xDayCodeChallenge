507d37d23f8f27b924f2bb417a612f0ad9eed77c
kevinngx <kevinngx625@gmail.com>
2021-08-16 20:19:59 +1000

    Day 36

--- Day_36/main.py
+++ Day_36/main.py
@@ -1,25 +1,28 @@
 import requests
 from datetime import timedelta
 from datetime import date
+import os
 
 def main():
     stock = "TSLA"
-    company_name = "Tesla Inc"
+    company_name = "Tesla"
     
     delta = getPerformance(stock)
     print("Price change = " + "{:.2%}".format(delta))
 
     if (delta > 0.005 or delta < -0.005): # (delta > 0.05 or delta < -0.05)
-        getNews(company_name)
-        sendText()
+        recent_news = getNews(company_name)
+        sendText(recent_news)
     
 
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 def getPerformance(stock):
     print("-- Stock Performance --")
-    AV_STOCK_KEY = "GW6E87JA3G9K7LCP"
+    os.environ["AV_STOCK_KEY"] = "GW6E87JA3G9K7LCP"
+    AV_STOCK_KEY = os.environ.get('AV_STOCK_KEY')
     AV_ENDPOINT = "https://www.alphavantage.co/query"
+    #print(AV_STOCK_KEY)
     api_params = {
         "function": "TIME_SERIES_DAILY",
         "symbol": stock,
@@ -36,35 +39,30 @@ def getPerformance(stock):
 
     day_1_price =  float(stock_data["Time Series (Daily)"][str(last_date)]["4. close"])
     day_2_price =  float(stock_data["Time Series (Daily)"][str(date_before)]["4. close"])
-    # print("Price on " + str(last_date) + ": " + str(day_1_price))
-    # print("Price on " + str(last_date) + ": " + str(day_2_price))
     return((day_1_price - day_2_price) / day_2_price)
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
 
-def getNews():
-    print("Stock News")
+def getNews(company_name):
+    print("-- Stock News --")
+    os.environ["NEWS_API_KEY"] = "8418834c55b74e61b04355d0291cacb7"
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    NEWS_API_ENDPOINT = "https://newsapi.org/v2/top-headlines"
+    api_params = {
+        "q": company_name,
+        "apiKey": NEWS_API_KEY
+    }
+
+    response = requests.get(NEWS_API_ENDPOINT, params=api_params).json()
+    return response['articles'][0]["title"]
 
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
 
 def sendText():
-    print("Sending Message")
-
-#Optional: Format the SMS message like this:
-# 
+    pass
 
 if __name__ == "__main__":
     main()
-
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 
