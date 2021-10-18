e0df2c9b7a153686a0c9881ee7bd879c337640fb
kevinngx <kevinngx625@gmail.com>
2021-10-19 00:23:32 +1100

    Day 45

--- Day_45/bs4-start/topmovies.py
+++ Day_45/bs4-start/topmovies.py
@@ -2,14 +2,22 @@ from bs4 import BeautifulSoup
 import requests
 
 def main():
-    response = requests.get("https://www.empireonline.com/movies/features/best-movies-2/")
+    response = requests.get("https://www.timeout.com/newyork/movies/best-movies-of-all-time")
     print(response)
     soup = BeautifulSoup(response.text, "html.parser")
-    all_movies = soup.find_all(name="h3", class_="title")
+    all_movies = soup.find_all(name="h3", class_="_h3_cuogz_1")
+    movie_titles = []
 
-    print(all_movies)
-    movie_titles = [movie.getText() for movie in all_movies]
-    movies = movie_titles[::-1]
+    for movie in all_movies:
+        # print(movie.getText())
+        movie_titles.append(movie.getText())
+    
+    movies = movie_titles[:100]
+    print(movies)
+
+    with open("movies.txt", mode="w") as file:
+        for movie in movies:
+            file.write(f'{movie}\n')
     
     
 
