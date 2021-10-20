c895916d5e2236da515b77c30b4aaba09b9f9fa5
kevinngx <kevinngx625@gmail.com>
2021-10-20 11:51:40 +1100

    Day_46 Complete

--- Day_46/main.py
+++ Day_46/main.py
@@ -4,49 +4,64 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from bs4 import BeautifulSoup
 
-
-SPOTIFY_BASE_ADDRESS = "https://api.spotify.com"
-BILLBOARD_URL = "https://www.billboard.com/charts/hot-100/2008-01-05"
-OAUTH_TOKEN = "BQDjNihy0ODOOyD6EFjgJC52pnWhR8Cvb7ldqeKj8bjCg2O9RSwQlPnEKyFsCegkUKhH55_wb1O8yHC1_UlK1eCmAcS6g3YE0y2A4UUVbA2zErLYmSpNBIxlcw6FrBnUmJHy2upll20nsUw"
-
 USER_ID = "1245266985"
-REDIRECT_URI = "http://127.0.0.1:9090"
-AUTH_URL = 'https://accounts.spotify.com/authorize'
-TOKEN_URL = 'https://accounts.spotify.com/api/token'
 
 def main():
-    scope = "playlist-modify-private" # Check privilege herehttps://developer.spotify.com/documentation/general/guides/scopes/
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-    print(sp.user_playlists(USER_ID))
+    target_date = input("Enter year in format YYYY-MM-DD : ")
+
+    playlist_uris = []
+    for song in getChart(target_date):
+        playlist_uris.append(getTrackURI(song))
+    print("Log | URIs Retrieved")
+
+    playlist_id  = createPlaylist(target_date)
+    insertTracks(playlist_uris, playlist_id)
+
+    print("Log | Program Completed")
+
+
+def getChart(target_date):
     
+    chart_url = "https://www.billboard.com/charts/hot-100/" + target_date
 
-def getChart():
-    response = requests.get(BILLBOARD_URL)
+    response = requests.get(chart_url)
     soup = BeautifulSoup(response.text, "html.parser")
-
     chart_songs = soup.find_all(name="span", class_="chart-element__information__song text--truncate color--primary")
-    chart_artists = soup.find_all(name="span", class_="chart-element__information__artist text--truncate color--secondary")
-
+    
     songs = []
-    artists = []
-
-    for artist in chart_artists:
-        artists.append(artist.getText())
-
     for song in chart_songs:
         songs.append(song.getText())
     
-    count = 0
-    for song in songs:
-        print(str(count + 1) + ". " + songs[count] + " - " + artists[count])
-        count += 1
+    print("Log | Top Charts Retrieved")
+    return songs
 
-def insertTrack():
-    print("Track Inserted")
+def insertTracks(songs, playlist_id):
+    scope = "playlist-modify-private" # Check privilege here https://developer.spotify.com/documentation/general/guides/scopes/
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    sp.playlist_add_items(playlist_id, songs)
 
-def createPlaylist():
+    print("Log | Tracks Inserted")
 
-    print("Playlist Created")
+def getTrackURI(song):
+    scope = "playlist-modify-private" # Check privilege herehttps://developer.spotify.com/documentation/general/guides/scopes/
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    response = sp.search(song, limit=1, type='track')
+    try:
+        URI = response["tracks"]["items"][0]["uri"]
+        print(f'URI Retrieved: {URI}')
+    except:
+        print(f'Song not found: {song}')
+        URI = "spotify:track:4cOdK2wGLETKBW3PvgPWqT"
+    return URI
+
+def createPlaylist(target_date):
+    scope = "playlist-modify-private" # Check privilege here https://developer.spotify.com/documentation/general/guides/scopes/
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    response = sp.user_playlist_create(USER_ID, "Throwback Charts - " + target_date, public=False)
+    playlist_id = response["id"]
+
+    print("Log | Playlist Created")
+    return playlist_id
 
 if __name__ == "__main__":
     main()
