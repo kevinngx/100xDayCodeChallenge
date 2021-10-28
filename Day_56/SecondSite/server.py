28f4900d277a858b9de2dd3c89d5206731c54766
kevinngx <kevinngx625@gmail.com>
2021-10-28 17:06:49 +1100

    Day 56 Complete

---
+++ Day_56/SecondSite/server.py
@@ -0,0 +1,10 @@
+import flask
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello(name=None):
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
